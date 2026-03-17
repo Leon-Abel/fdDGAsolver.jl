acd5324b3c2fc916964e7a7306fd9c915830f5d5
Leon Abel <leonabel00@gmail.com>
2026-03-17 10:21:39 +0100

    Update symmetries.jl
    Added comments to symmetries.jl

--- src/symmetries.jl
+++ src/symmetries.jl
@@ -1,13 +1,37 @@
+# symmetries.jl
+#
+# Symmetry transformations for Green's functions and vertices:
+#   - self-energy symmetries (sΣ)
+#   - particle-particle channel symmetries (sK1pp, sK2pp, sK3pp)
+#   - particle-hole channel symmetries (sK1ph, sK2ph, sK3ph)
+#
+# These functions return a transformed frequency NTuple and an `Operation` 
+# describing sign changes (sgn) or complex conjugation (con).
+
 # self-energy symmetries
+#----------------------------------------------------------------------------------------------#
+"""
+    function sΣ(w::NTuple{1, MatsubaraFrequency},)::Tuple{NTuple{1, MatsubaraFrequency}, Operation}
+
+Time reversal symmetry for the self-energy 'Σ': 'Σ(iw) = Σ*(-iw)'.
+Since the implementation stores iΣ, the relation transforms as 'iΣ(iw) = -(iΣ)*(-iw)'.
+"""
 function sΣ(
     w :: NTuple{1, MatsubaraFrequency},
     ) :: Tuple{NTuple{1, MatsubaraFrequency}, Operation}
 
+    # Maps w -> -w, flips sign, and applies conjugation.
     # We store iΣ, so the symmetry Σ -> Σ* becomes iΣ -> -(iΣ)*.
     return (-w[1],), Operation(sgn = true, con = true)
 end
 
-# particle-particle symmetries
+# particle-particle (pp) symmetries
+#----------------------------------------------------------------------------------------------#
+"""
+    function sK1pp(w::NTuple{1, MatsubaraFrequency},)::Tuple{NTuple{1, MatsubaraFrequency}, Operation}
+
+pp-channel K1 class symmetry: K1pp(iw) = K1pp*(-iw)
+"""
 function sK1pp(
     w :: NTuple{1, MatsubaraFrequency},
     ) :: Tuple{NTuple{1, MatsubaraFrequency}, Operation}
@@ -15,6 +39,11 @@ function sK1pp(
     return (-w[1],), Operation(sgn = false, con = true)
 end
 
+"""
+    function sK2pp1(w::NTuple{2, MatsubaraFrequency},)::Tuple{NTuple{2, MatsubaraFrequency}, Operation}
+
+pp-channel K2 class symmetry (1): K2pp(iw1, iw2) = K2pp*(-iw1, -iw2)
+"""
 function sK2pp1(
     w :: NTuple{2, MatsubaraFrequency},
     ) :: Tuple{NTuple{2, MatsubaraFrequency}, Operation}
@@ -22,6 +51,11 @@ function sK2pp1(
     return (-w[1], -w[2]), Operation(sgn = false, con = true)
 end
 
+"""
+    function sK2pp2(w::NTuple{2, MatsubaraFrequency},)::Tuple{NTuple{2, MatsubaraFrequency}, Operation}
+
+pp-channel K2 class symmetry (2): K2pp(iw1, iw2) = K2pp(iw1, iw1 - iw2)
+"""
 function sK2pp2(
     w :: NTuple{2, MatsubaraFrequency},
     ) :: Tuple{NTuple{2, MatsubaraFrequency}, Operation}
@@ -29,6 +63,11 @@ function sK2pp2(
     return (w[1], w[1] - w[2]), Operation()
 end
 
+"""
+    function sK3pp1(w::NTuple{3, MatsubaraFrequency},)::Tuple{NTuple{3, MatsubaraFrequency}, Operation}
+
+pp-channel K3 class symmetry (1): K3pp(iw1, iw2, iw3) = K3pp*(-iw1, -iw2, -iw3)
+"""
 function sK3pp1(
     w :: NTuple{3, MatsubaraFrequency},
     ) :: Tuple{NTuple{3, MatsubaraFrequency}, Operation}
@@ -36,6 +75,11 @@ function sK3pp1(
     return (-w[1], -w[2], -w[3]), Operation(sgn = false, con = true)
 end
 
+"""
+    function sK3pp2(w::NTuple{3, MatsubaraFrequency},)::Tuple{NTuple{3, MatsubaraFrequency}, Operation}
+
+pp-channel K3 class symmetry (2): K3pp(iw1, iw2, iw3) = K3pp(iw1, iw3, iw2)
+"""
 function sK3pp2(
     w :: NTuple{3, MatsubaraFrequency},
     ) :: Tuple{NTuple{3, MatsubaraFrequency}, Operation}
@@ -43,6 +87,11 @@ function sK3pp2(
     return (w[1], w[3], w[2]), Operation()
 end
 
+"""
+    function sK3pp3(w::NTuple{3, MatsubaraFrequency},)::Tuple{NTuple{3, MatsubaraFrequency}, Operation}
+
+pp-channel K3 class symmetry (3): K3pp(iw1, iw2, iw3) = K3pp(iw1, iw1 - iw2, iw1 - iw3)
+"""
 function sK3pp3(
     w :: NTuple{3, MatsubaraFrequency},
     ) :: Tuple{NTuple{3, MatsubaraFrequency}, Operation}
@@ -51,6 +100,13 @@ function sK3pp3(
 end
 
 # particle-hole symmetries
+#----------------------------------------------------------------------------------------------#
+# many identical to pp symmetries
+"""
+    function sK1ph(w::NTuple{1, MatsubaraFrequency},)::Tuple{NTuple{1, MatsubaraFrequency}, Operation}
+
+ph-channel K1 class symmetry: K1ph(iw) = K1ph*(-iw)
+"""
 function sK1ph(
     w :: NTuple{1, MatsubaraFrequency},
     ) :: Tuple{NTuple{1, MatsubaraFrequency}, Operation}
@@ -58,6 +114,11 @@ function sK1ph(
     return sK1pp(w)
 end
 
+"""
+    function sK2ph1(w::NTuple{2, MatsubaraFrequency},)::Tuple{NTuple{2, MatsubaraFrequency}, Operation}
+
+ph-channel K2 class symmetry (1): K2ph(iw1, iw2) = K2ph*(-iw1, -iw2)
+"""
 function sK2ph1(
     w :: NTuple{2, MatsubaraFrequency},
     ) :: Tuple{NTuple{2, MatsubaraFrequency}, Operation}
@@ -65,6 +126,11 @@ function sK2ph1(
     return sK2pp1(w)
 end
 
+"""
+    function sK2ph2(w::NTuple{2, MatsubaraFrequency},)::Tuple{NTuple{2, MatsubaraFrequency}, Operation}
+
+ph-channel K2 class symmetry (2): K2pp(iw1, iw2) = K2pp(-iw1, iw1 + iw2)
+"""
 function sK2ph2(
     w :: NTuple{2, MatsubaraFrequency},
     ) :: Tuple{NTuple{2, MatsubaraFrequency}, Operation}
@@ -72,6 +138,11 @@ function sK2ph2(
     return (-w[1], w[1] + w[2]), Operation()
 end
 
+"""
+    function sK3ph1(w::NTuple{3, MatsubaraFrequency},)::Tuple{NTuple{3, MatsubaraFrequency}, Operation}
+
+ph-channel K3 class symmetry (1): K3php(iw1, iw2, iw3) = K3ph*(-iw1, -iw2, -iw3)
+"""
 function sK3ph1(
     w :: NTuple{3, MatsubaraFrequency},
     ) :: Tuple{NTuple{3, MatsubaraFrequency}, Operation}
@@ -79,6 +150,11 @@ function sK3ph1(
     return sK3pp1(w)
 end
 
+"""
+    function sK3ph2(w::NTuple{3, MatsubaraFrequency},)::Tuple{NTuple{3, MatsubaraFrequency}, Operation}
+
+ph-channel K3 class symmetry (2): K3ph(iw1, iw2, iw3) = K3ph(iw1, iw3, iw2)
+"""
 function sK3ph2(
     w :: NTuple{3, MatsubaraFrequency},
     ) :: Tuple{NTuple{3, MatsubaraFrequency}, Operation}
@@ -86,6 +162,11 @@ function sK3ph2(
     return sK3pp2(w)
 end
 
+"""
+    function sK3ph3(w::NTuple{3, MatsubaraFrequency},)::Tuple{NTuple{3, MatsubaraFrequency}, Operation}
+
+ph-channel K3 class symmetry (3): K3ph(iw1, iw2, iw3) = K3ph(-iw1, iw1 + iw2, iw1 + iw3)
+"""
 function sK3ph3(
     w :: NTuple{3, MatsubaraFrequency},
     ) :: Tuple{NTuple{3, MatsubaraFrequency}, Operation}
